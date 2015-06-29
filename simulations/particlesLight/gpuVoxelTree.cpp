fafbebabd334b84e4281c8f598a8aa1ddf811139
Zakkai Davidson <zdavidson@hmc.edu>
2015-06-29 15:28:57 -0700

    added data structure file to represent voxels on the GPU

---
+++ simulations/particlesLight/gpuVoxelTree.cpp
@@ -0,0 +1,16 @@
+/* gpuVoxelTree.cpp
+ *
+ * Author: Zakkai Davidson - zdavidson@hmc.edu
+ * Date: 29 Jun 2015
+ * 
+ * Purpose: The class included in this file serves to represent a voxel based
+ *          3D model on the GPU similar to that of OpenVDB. The data structure
+ *          consists of tiers, each of which splits up the active space of
+ *          interest into different sized cells. The lowest level contains the
+ *          actual voxel data. The design of this data structure is simply to
+ *          answer the question "is particle x inside a voxel?", and thus it is
+ *          meant to be used primarily with particle simulations on the GPU.
+ *
+ */ 
+
+
